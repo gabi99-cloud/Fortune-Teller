1a053b309bee2d4c3675e5c06102d507ec99b834
gabi99-cloud <80900090+gabi99-cloud@users.noreply.github.com>
2021-03-23 14:15:57 -0500

    Add files via upload

---
+++ Fortune.js
@@ -0,0 +1,39 @@
+function conditionalTest(userInput) {
+    let results;
+    // Check if name is equal to ____
+    if (userInput === 'yes') {
+        results = "fortune";
+        console.log(fortune);
+    } else {userInput === 'no')
+        results = "messages";
+        console.log(messages);
+
+
+
+const userInput = window.prompt('AM I THE ONE, yes or no ?');
+
+let userInput= window.prompt('AM I THE ONE, yes or no ?');
+userInput = userInput.toLowerCase(yes, no);
+    fortunes = ['YOU ARE NUMBER 2', 'YOU ARE AN AGENT', 'YOU JUST WANTED A COOKIE', 'YOU KNEW YOU WERENT BEFORE YOU GOT HERE', 'IT SUCKS TO SUCK'];
+    messages = ['YOU ARE THE ONE ALEXA', 'FOLLOW THE WHITE RABBIT', 'MORPHEUS AWAITS', 'I TOLD YOU EXACTLY WHAT YOU NEEDED TO HEAR NOTHING MORE']; 
+    }
+    if (userInput === 'yes') {
+        alert(fortunes[Math.floor(Math.random() 5 *fortunes.length)]);
+    } else { (userInput === 'no') {
+         alert(messages[Math.floor(Math.random(1)*fortunes.5])
+       alert("LEAN BACK FAR AF");
+    }
+
+function messages (userInput, messages) {
+    userInput = userInput.toLowerCase();
+    messages = ['YOU ARE THE ONE ALEXA', 'FOLLOW THE WHITE RABBIT', 'MORPHEUS AWAITS', 'I TOLD YOU EXACTLY WHAT YOU NEEDED TO HEAR NOTHING MORE']
+    if (userInput === 'no') {
+        alert(messages[Math.floor(no: 1)*messages.5])
+    }
+    alert("YOU TRIED IT");
+}
+
+
+
+
+
